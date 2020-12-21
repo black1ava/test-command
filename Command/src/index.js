d08a832e0b0917e5b71f477ffb786c14f9617624
Black1ava <roeunboratharath@gmail.com>
2020-12-21 14:10:59 +0700

    Add index.js

--- Command/src/index.js
+++ Command/src/index.js
@@ -1,11 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <h1>Hello</h1>,
   document.getElementById('app')
-);
+)
