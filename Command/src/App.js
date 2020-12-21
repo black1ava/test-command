4dea9771da77fd8f3562e84c11356cb732ceb9c9
Black1ava <roeunboratharath@gmail.com>
2020-12-21 14:23:24 +0700

    Add files
    App.js, Body.js, Header.js

---
+++ Command/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Header from './Component/Header'
+import Body from './Component/Body'
+export default function App(){
+  return(
+    <div>
+      <Header />
+      <Body />
+    </div>
+  )
+}
---
+++ Command/src/Component/Body.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Body(){
+  return (
+    <div>
+      <p>Test is finished</p>
+    </div>
+  )
+}
